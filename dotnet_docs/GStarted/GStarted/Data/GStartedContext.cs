61a86f8dd4617813a5c9bec08dc0f64ae1b9ed96
Chandrakanth <chan18@users.noreply.github.com>
2025-02-07 21:35:16 +0530

    simple app

---
+++ dotnet_docs/GStarted/GStarted/Data/GStartedContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using GStarted.Modles;
+
+namespace GStarted.Data
+{
+    public class GStartedContext : DbContext
+    {
+        public GStartedContext (DbContextOptions<GStartedContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<GStarted.Modles.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ dotnet_docs/GStarted/GStarted/Pages/Movies/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using GStarted.Data;
+using GStarted.Modles;
+
+namespace GStarted.Pages.Movies
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly GStarted.Data.GStartedContext _context;
+
+        public DetailsModel(GStarted.Data.GStartedContext context)
+        {
+            _context = context;
+        }
+
+        public Movie Movie { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movie.FirstOrDefaultAsync(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Movie = movie;
+            }
+            return Page();
+        }
+    }
+}
