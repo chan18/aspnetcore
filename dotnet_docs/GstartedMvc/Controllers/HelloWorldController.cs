5636b2a1b38305978a76da5cd3203d537632a146
Chandrakanth <chan18@users.noreply.github.com>
2025-02-08 04:10:04 +0530

    mvc play

---
+++ dotnet_docs/GstartedMvc/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace GstartedMvc.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            //// HtmlEncoder.Default.Encode protects the app from default inputs.
+            //return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
---
+++ dotnet_docs/GstartedMvc/Data/GstartedMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using GstartedMvc.Models;
+
+namespace GstartedMvc.Data
+{
+    public class GstartedMvcContext : DbContext
+    {
+        public GstartedMvcContext (DbContextOptions<GstartedMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; } = default!;
+    }
+}
